4b0a7a7eb76994edd0e945b63e19afddba6f6ff6
jwlkns <111277602+jwlkns@users.noreply.github.com>
2023-09-21 17:24:06 +0200

    Update streamlit_app.py
    hide fruit load list behind button

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,14 @@ def get_fruityvice_data(fruit: str):
   # Send request
   response = requests.get(f"https://fruityvice.com/api/fruit/{fruit}")
   # Normalize and return api response
-  return pandas.json_normalize(response.json())
+  return pandas.json_normalize(response.json())  
+
+def get_fruit_load_list():
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -42,15 +49,9 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button("Get Fruit Load List"):
+  streamlit.header("The fruit load list contains:")
+  streamlit.dataframe(get_fruit_load_list())
 
 add_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_fruit)
