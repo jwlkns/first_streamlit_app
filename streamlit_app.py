04c079a3c6d405c7e9fd4f9d002c4746bb57cba4
jwlkns <111277602+jwlkns@users.noreply.github.com>
2023-09-21 17:12:11 +0200

    Update streamlit_app.py
    error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,14 +25,20 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
-# Normalize api response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Dsiplay api response as table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    
+    fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+    # Normalize api response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Dsiplay api response as table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
